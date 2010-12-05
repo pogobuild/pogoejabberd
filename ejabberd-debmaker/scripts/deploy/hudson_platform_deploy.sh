c6b4c0482c678425238eaf0482d5fc5fb60b467f
Hudson_build_box <drmightyman@gmail.com>
2010-12-04 21:49:52 -0800

    changes from p4

--- ejabberd-debmaker/scripts/deploy/hudson_platform_deploy.sh
+++ ejabberd-debmaker/scripts/deploy/hudson_platform_deploy.sh
@@ -6,17 +6,17 @@ check_error()
   fi
 }
 
-cd $WORKSPACE/pogofacebook/debmaker
-if [ -e debian-pkg.tar ];then
-  rm -rf debian-pkg.tar
+cd $WORKSPACE/pogotools/ejabberd-debmaker
+if [ -e ejabberd-pkg.tar ];then
+  rm -rf ejabberd-pkg.tar
 fi
 
 # no longer needed as platform no longer builds genius-app's
-#$WORKSPACE/pogofacebook/debmaker/scripts/version_profiles.sh "debmaker/data/versions.tdd"
+#$WORKSPACE/pogotools/ejabberd-debmaker/scripts/version_profiles.sh "debmaker/data/versions.tdd"
 
-cd $WORKSPACE/pogofacebook/debmaker
+cd $WORKSPACE/pogotools/ejabberd-debmaker
 
-tar zcvf debian-pkg.tar * > /dev/null
+tar zcvf ejabberd-pkg.tar * > /dev/null
 #--exclude "gamebrick" --exclude "pogoweb" --exclude "psd" --exclude "srvbase" --exclude "userservice" --exclude ".p4config" --exclude "pom.xml" --exclude "setenv.bat" --exclude "settings.xml"
 
 
